126b5e6955e372e9f5c75b54bd6ac81376ad2b35
saleemjavid36 <102580058+saleemjavid36@users.noreply.github.com>
2025-04-11 20:21:43 +0530

    addedInputFieldTOcustomCOunt

--- src/components/mocgenerator/MocGenerator.js
+++ src/components/mocgenerator/MocGenerator.js
@@ -15,100 +15,106 @@ import { CarsData } from "../../data/carDetails";
 
 
 const MocGenerator = () => {
-  const uuid = uuidv4();
-  //  useEffect(()=>{
-  //    console.log(UserProfessions)
-  //  })
+  const [totalRecordsCount, setTotalRecordsCount] = useState(10)  
+  const generateMockData = (count) => {
+    if (count > 100000) {
+      alert("Too much data to download. Please reduce the number of records.");
+    } else {
+      const createRecord = (recordId) => {
+        const uuid = uuidv4();
+        const name = namesData[Math.floor(Math.random() * namesData.length)]
+        const userRandomMails = generateEmailAddresses(name)
+        const country = countriesFullData[Math.floor(Math.random() * countriesFullData.length)]
+        const stateProvinces =
+          country.stateProvinces && country.stateProvinces.length > 0
+            ? country.stateProvinces[Math.floor(Math.random() * country.stateProvinces.length)].name
+            : null;
+        const userCarData = CarsData[Math.floor(Math.random() * CarsData.length)]
+        return {
+          _id: `ObjectId(${uuid})`,
+          id: Number(recordId),
+          name: name,
+          phone: `${country.phone}-${phoneNumbers[Math.floor(Math.random() * phoneNumbers.length)]}`,
+          postalZip: `${postalCodes[Math.floor(Math.random() * postalCodes.length)]}`,
+          country: country.name,
+          stateProvinces: stateProvinces,
+          email: userRandomMails[Math.floor(Math.random() * userRandomMails.length)],
+          address: `${Math.floor(Math.random() * 9000 + 100)} Street`,
+          salary: Number((Math.random() * 10000 + 1).toFixed(2)),
+          currencySymbol: `${country.symbol}`,
+          profession: UserProfessions[Math.floor(Math.random() * UserProfessions.length)],
+          hobbies: Hobbies[Math.floor(Math.random() * Hobbies.length)],
+          userPreferences: [
+            {
+              dishesDetails:
+              {
+                favoriteDish: Dishes[Math.floor(Math.random() * Dishes.length)],
+                cuisineType: CuisineTypes[Math.floor(Math.random() * CuisineTypes.length)],
+                cookingTime: `${Math.floor(Math.random() * 90 + 10)} minutes`,
+                ingredients: [Ingredients[Math.floor(Math.random() * 3)], Ingredients[Math.floor(Math.random() * 3)],],
+              }
+              ,
+              plantDetails:
+              {
+                plantType: PlantTypes[Math.floor(Math.random() * PlantTypes.length)],
+                name: PlantNames[Math.floor(Math.random() * PlantNames.length)],
+                careTips: CareTips[Math.floor(Math.random() * CareTips.length)],
+                origin: Origins[Math.floor(Math.random() * Origins.length)],
+                indoorFriendly: Math.random() < 0.5,
+              },
 
-  const generateMockData = () => {
-    const createRecord = (recordId) => {
-      const name = namesData[Math.floor(Math.random() * namesData.length)]
-      const userRandomMails = generateEmailAddresses(name)
-      const country = countriesFullData[Math.floor(Math.random() * countriesFullData.length)]
-      const stateProvinces =
-        country.stateProvinces && country.stateProvinces.length > 0
-          ? country.stateProvinces[Math.floor(Math.random() * country.stateProvinces.length)].name
-          : null;
-      const userCarData = CarsData[Math.floor(Math.random() * CarsData.length)]
-      return {
-        _id: `ObjectId(${uuid})`,
-        id: Number(recordId),
-        name: name,
-        phone: `${country.phone}-${phoneNumbers[Math.floor(Math.random() * phoneNumbers.length)]}`,
-        postalZip: `${postalCodes[Math.floor(Math.random() * postalCodes.length)]}`,
-        country: country.name,
-        stateProvinces: stateProvinces,
-        email: userRandomMails[Math.floor(Math.random() * userRandomMails.length)],
-        address: `${Math.floor(Math.random() * 9000 + 100)} Street`,
-        salary: Number((Math.random() * 10000 + 1).toFixed(2)),
-        currencySymbol: `${country.symbol}`,
-        profession: UserProfessions[Math.floor(Math.random() * UserProfessions.length)],
-        hobbies: Hobbies[Math.floor(Math.random() * Hobbies.length)],
-        userPreferences: [
-          {
-            dishesDetails:
-            {
-              favoriteDish: Dishes[Math.floor(Math.random() * Dishes.length)],
-              cuisineType: CuisineTypes[Math.floor(Math.random() * CuisineTypes.length)],
-              cookingTime: `${Math.floor(Math.random() * 90 + 10)} minutes`,
-              ingredients: [Ingredients[Math.floor(Math.random() * 3)], Ingredients[Math.floor(Math.random() * 3)],],
-            }
-            ,
-            plantDetails:
-            {
-              plantType: PlantTypes[Math.floor(Math.random() * PlantTypes.length)],
-              name: PlantNames[Math.floor(Math.random() * PlantNames.length)],
-              careTips: CareTips[Math.floor(Math.random() * CareTips.length)],
-              origin: Origins[Math.floor(Math.random() * Origins.length)],
-              indoorFriendly: Math.random() < 0.5,
+              carDetails:
+              {
+                carBrand: Object.keys(userCarData)[0],
+                model: userCarData[Object.keys(userCarData)[0]]['models'][Math.floor(
+                  Math.random() * userCarData[Object.keys(userCarData)[0]]['models'].length
+                )],
+                year: userCarData[Object.keys(userCarData)[0]]['years'][Math.floor(
+                  Math.random() * userCarData[Object.keys(userCarData)[0]]['years'].length
+                )],
+                features: [
+                  userCarData[Object.keys(userCarData)[0]]['features'][Math.floor(
+                    Math.random() * userCarData[Object.keys(userCarData)[0]]['features'].length
+                  )],
+                  userCarData[Object.keys(userCarData)[0]]['features'][Math.floor(
+                    Math.random() * userCarData[Object.keys(userCarData)[0]]['features'].length
+                  )],
+                ],
+                electric: Math.random() < 0.5,
+              },
+
             },
 
-            carDetails:
-            {
-              carBrand: Object.keys(userCarData)[0],
-              model: userCarData[Object.keys(userCarData)[0]]['models'][Math.floor(
-                Math.random() * userCarData[Object.keys(userCarData)[0]]['models'].length
-              )],
-              year: userCarData[Object.keys(userCarData)[0]]['years'][Math.floor(
-                Math.random() * userCarData[Object.keys(userCarData)[0]]['years'].length
-              )],
-              features: [
-                userCarData[Object.keys(userCarData)[0]]['features'][Math.floor(
-                  Math.random() * userCarData[Object.keys(userCarData)[0]]['features'].length
-                )],
-                userCarData[Object.keys(userCarData)[0]]['features'][Math.floor(
-                  Math.random() * userCarData[Object.keys(userCarData)[0]]['features'].length
-                )],
-              ],
-              electric: Math.random() < 0.5,
-            },
-
-          },
-
-        ],
+          ],
+        };
       };
-    };
-    // Generate 10,000 records
-    const data = [];
-    for (let i = 1; i <= 1000; i++) {
-      data.push(createRecord(i));
+      const data = [];
+      for (let i = 1; i <= count; i++) {
+        data.push(createRecord(i));
+      }
+      const json = JSON.stringify(data, null, 2);
+      const blob = new Blob([json], { type: "application/json" });
+      const link = document.createElement("a");
+      link.href = URL.createObjectURL(blob);
+      link.download = "User-Mock-Data.json";
+      link.click();
     }
-// console.log(data[0])
 
-    // Save the data to a file
-    const json = JSON.stringify(data, null, 2);
-    const blob = new Blob([json], { type: "application/json" });
-    const link = document.createElement("a");
-    link.href = URL.createObjectURL(blob);
-    link.download = "mock_data.json";
-    link.click();
   };
+  const handleInputChange = (e) => {
+    const {value} = e.target;
+    setTotalRecordsCount(Number(value));
+  }
 
   return (
     <div className="col-12 d-flex justify-content-center   vh-100">
       <div className="my-5 d-flex flex-column align-items-center">
+        <div>
+          <span>Number of records:  </span>
+          <input value={totalRecordsCount ?? 0} type="number" onChange={handleInputChange} />
+        </div>
         <h1>Generate Mock JSON Data</h1>
-        <button onClick={generateMockData}>Generate and Download JSON</button>
+        <button onClick={() => generateMockData(totalRecordsCount)}>Generate and Download JSON</button>
       </div>
 
     </div>
